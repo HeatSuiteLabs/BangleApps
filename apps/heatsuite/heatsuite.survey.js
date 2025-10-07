663ecc9bb23c7d3ff1d2152bdbbd1630ceb44375
nravanelli <nick.ravanelli@gmail.com>
2025-10-07 15:16:02 +0800

    heatsuite v0.12

--- apps/heatsuite/heatsuite.survey.js
+++ apps/heatsuite/heatsuite.survey.js
@@ -26,7 +26,6 @@ function shuffle(array) {
   return result;
 }
 
-
 function log(msg) {
   if (!settings.DEBUG) {
     return;
@@ -48,23 +47,23 @@ function queueTaskScreenTimeout() {
 var scrollInterval;
 function drawScrollingText(text,height) {
   Bangle.appRect = { x: 0, y: height, w: g.getWidth(), h: g.getHeight() - height, x2: g.getWidth()-1, y2: g.getHeight()-1 };
-  let stringWidth = g.stringWidth(text);
-  let textX = (stringWidth > g.getWidth())? (stringWidth/2) : 0;
   g.setColor("#000");
   g.setBgColor("#FFF");
-  g.setFont("6x8:2", 2);
+  g.setFont("Vector", 28);
   g.clearRect(0, 0, g.getWidth(), height);
+  let stringWidth = g.stringWidth(text);
+  let textX = (stringWidth > g.getWidth())? (stringWidth/2) + 30 : 0;
   function QuestionText() {
     g.setColor("#000");
     g.setBgColor("#FFF");
-    g.setFont("6x8:2", 2); 
+    g.setFont("Vector", 28); 
     g.clearRect(0, 0, g.getWidth(), height);
     g.drawString(text, textX, height/2); 
     textX -= 5;
-    if (textX < (-(stringWidth/2)+g.getWidth())) textX = (stringWidth/2);
+    if (textX < (-(stringWidth/2)+g.getWidth() - 30)) textX = (stringWidth/2) + 30;
     g.flip();
   }
-  if(scrollInterval) clearTimeout(scrollInterval);
+  if(scrollInterval) clearInterval(scrollInterval);
   if(stringWidth > g.getWidth()){
     scrollInterval = setInterval(QuestionText, 60); //will need to scroll as its too long
   }else{
@@ -72,14 +71,13 @@ function drawScrollingText(text,height) {
   }
 }
 function drawResponseOpts(question){
+  g.clear();
   //force scrolling of question at the top
   drawScrollingText(question.text[lang].replace(/\\n/g, " "),30);
   var height = 30;
   var options = question.options;
-  if(options.length < 4){
-    height = Math.floor(Bangle.appRect.h / options.length);
-  }
-  let drawItem = function (idx,r){
+  if(Array.isArray(options)){
+    let drawItem = function (idx,r){
     var optionText = options[idx].value;
     if (options[idx].text !== undefined) {
       optionText = options[idx].text[lang];
@@ -87,7 +85,7 @@ function drawResponseOpts(question){
     g.setColor((options[idx].color)?options[idx].color:"#000");
     g.setBgColor((options[idx].btnColor)?options[idx].btnColor:"#CCC").clearRect(r.x,r.y,r.x+r.w-1,r.y+r.h-1);
     g.setFontAlign(0, 0, 0);
-    g.setFont("6x8:2").drawString(optionText,r.x+(g.getWidth()/2),r.y+(height/2));
+    g.setFont("Vector", 28).drawString(optionText,r.x+(g.getWidth()/2),r.y+(height/2));
   };
   let selectItem = function(id) {
     var resp = (options[id] && options[id].text && lang in options[id].text) ? options[id].text[lang] : options[id].value;
@@ -96,13 +94,66 @@ function drawResponseOpts(question){
     const cbString = question.key + "," + resp + "," + options[id].value + "," + next;
     return surveyResponse(cbString);
   };
+  if(options.length < 5){
+    height = Math.floor(Bangle.appRect.h / options.length);
+  }
   E.showScroller({
     h : height,
     c : options.length,
     draw : drawItem,
     select : selectItem
   });
+  }else{
+    //This is the new option handling for different option types
+    var currentOpt = (options.startOpt != undefined) ? options.startOpt : options.min;
+    var minOpt = (options.min != undefined) ? options.min : 0;
+    var maxOpt = (options.max != undefined) ? options.max : 10;
+    var optStep = (options.optStep != undefined) ? options.optStep : 1;
+    var units = (options.units != undefined) ? options.units : undefined;
+    var next = (options.next != undefined) ? options.next : {};
+    const getValueIfKeyExists = (obj, key) => (obj && typeof obj === "object" && key in obj ? obj[key] : 0);
+    let handleResponse = function (){
+      var value = currentOpt;
+      if(units) value += units;
+      const n = getValueIfKeyExists(next, currentOpt);
+      const cbString = question.key + "," + currentOpt + "," + value + "," + n;
+      return surveyResponse(cbString);
+    };
+    let optionDown = function() {
+      const nextOpt = currentOpt - optStep;
+      currentOpt = (nextOpt < minOpt) ? maxOpt : nextOpt;
+      layout.response.label = currentOpt;
+      var label = currentOpt;
+      if(units) label += units;
+      layout.response.label = label;
+      layout.clear(layout.response);
+      layout.render(layout.response);
+    };
+    let optionUp = function() {
+      const nextOpt = currentOpt + optStep;
+      currentOpt = (nextOpt > maxOpt) ? minOpt : nextOpt;
+      var label = currentOpt;
+      if(units) label += units;
+      layout.response.label = label;
+      layout.clear(layout.response);
+      layout.render(layout.response);
+    }
+    var Layout = require("Layout");
+    var initLabel = (units != undefined) ? currentOpt + units : currentOpt;
+    var layout = new Layout( {
+      type:"v", c: [
+        {type:"txt", font:"50%", label:initLabel, fillx:1, id: "response", cb: l=>handleResponse() },
+        {type:"h", c: [
+          {type:"btn", font:"15%", label:"<<", fillx:1, cb: l=>optionDown() },
+          {type:"txt", font:"15%", label:"  ", fillx:1 },
+          {type:"btn", font:"15%", label:">>", fillx:1, cb: l=>optionUp() }
+        ]},
+      ], lazy:true});
+    layout.render();
+  }
+  return;
 }
+
 function drawSurveyLayout(question) {
   if(scrollInterval) clearTimeout(scrollInterval);
   if (surveyFile === undefined) {
@@ -126,7 +177,7 @@ function drawSurveyLayout(question) {
       },
       {
         type: "txt",
-        font: "6x8:3",
+        font: "30%",
         label: "Done!"
       }]
     });
@@ -182,12 +233,12 @@ function drawSurveyLayout(question) {
     wrap: true,
     fillx: 1,
     filly: 1,
-    font: "6x8:2",
+    font: "15%",
     label: questionText,
     id: "label"
   };
   out.c.push(q);
-  var optFont = '6x8:3';
+  var optFont = '15%';
   if (question.optFont !== undefined) optFont = question.optFont;
   var opt = {
     type: "btn",
@@ -220,7 +271,7 @@ function surveyResponse(text) {
   }
 }
 
-var surveyFile = require('Storage').readJSON(surveyFileJSON, true) || {"questions":[{"text":{"en_GB":"Thermal Comfort?"},"options":[{"text":{"en_GB":"Comfortable"},"value":0,"color":"#ffffff","btnColor":"#38ed35"},{"text":{"en_GB":"Uncomfortable"},"value":1,"color":"#ffffff","btnColor":"#ff0019"}],"tod":[[0,2359]],"key":"comfort"}],"supported":{"en_GB":"English (GB)"}};
+var surveyFile = require('Storage').readJSON(surveyFileJSON, true);
 var QArr = (surveyFile.questions || []).filter(q => q.followup !== true);
 if (settings.surveyRandomize !== undefined && settings.surveyRandomize) {
   QArr = shuffle(QArr);
