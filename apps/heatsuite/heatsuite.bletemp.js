84147d4e6f0db34174ebab9dd00c473611e3cb12
nravanelli <nick.ravanelli@gmail.com>
2026-04-13 22:40:00 +0800

    updated heatsuite.bletemp.js with logging for luc to test

--- apps/heatsuite/heatsuite.bletemp.js
+++ apps/heatsuite/heatsuite.bletemp.js
@@ -4,11 +4,23 @@ var layout;
 
 var settings = modHS.getSettings();
 //var appCache = modHS.getCache();
-function log(msg) {
+function log() {
   if (!settings.DEBUG) {
     return;
   } else {
-    console.log(msg);
+    var msg = Array.prototype.slice.call(arguments).map(function (v) {
+      if (v === undefined) return "undefined";
+      if (v === null) return "null";
+      if (typeof v === "object") {
+        try {
+          return JSON.stringify(v);
+        } catch (e) {
+          return "[object]";
+        }
+      }
+      return "" + v;
+    }).join(" ");
+    modHS.log(msg);
   }
 }
 
@@ -26,6 +38,7 @@ const Schema_ThermistorPodBLE = {
 };
 
 function getTcore(id) {
+  log("[bletemp] getTcore start", id);
   layout = new Layout({
     type: "v", c: [
       {
@@ -52,29 +65,45 @@ function getTcore(id) {
     "measures": []
   };
   NRF.connect(id).then(function (g) {
+    log("[bletemp] connected", id);
     gatt = g;
     startTime = parseInt((getTime()).toFixed(0));
     gatt.device.on('gattserverdisconnected', function (reason) {
       gatt = null;
       Bangle.load();
-      log("Disconnected ", reason);
+      log("[bletemp] disconnected", reason);
     });
+    log("[bletemp] requesting service", "1809");
     return gatt.getPrimaryService("1809");
   }).then(function (s) {
+    log("[bletemp] service ready");
+    log("[bletemp] requesting characteristic", "00002A1F-0000-1000-8000-00805F9B34FB");
     return s.getCharacteristic("00002A1F-0000-1000-8000-00805F9B34FB");
   }).then(function (c) {
+    log("[bletemp] characteristic ready, listening for notifications");
     c.on('characteristicvaluechanged', function (event) {
+      log("[bletemp] notification received");
       const receivedData = modHS.parseBLEData(event.target.value, Schema_ThermistorPodBLE);
+      log("[bletemp] parsed sample", {
+        adc: receivedData.adc,
+        temp: receivedData.temp,
+        tempAvg: receivedData.tempAvg,
+        ta: receivedData.ta,
+        rh: receivedData.rh
+      });
       TCoreData.temp = receivedData.tempAvg;
       TCoreData.ta = receivedData.ta;
       TCoreData.rh = receivedData.rh;
       TCoreData.measures.push(receivedData.adc);
       var timeNow = parseInt((getTime()).toFixed(0));
       var diff = timeNow - startTime;
+      log("[bletemp] elapsed", diff, "seconds");
       var display;
       if (diff > 90 && !complete) { // time to save the data and disconnect
         complete = true;
+        log("[bletemp] measurement window complete, saving");
         if (modHS.saveDataToFile('coreTemp', 'coreTemperature', TCoreData)) {
+          log("[bletemp] save success");
           display = {
             type: "v", c: [
               {
@@ -84,10 +113,12 @@ function getTcore(id) {
               }
             ]
           };
-
+        } else {
+          log("[bletemp] save failed");
         }
       } else {
         var remaining = 90 - diff;
+        log("[bletemp] waiting", remaining, "seconds remaining");
         display = {
           type: "v", c: [
             {
@@ -107,15 +138,23 @@ function getTcore(id) {
       g.clear();
       layout.render();
       if (complete) {
+        log("[bletemp] cleanup start");
         if(gatt){
+          log("[bletemp] disconnecting gatt");
           gatt.disconnect();
+        } else {
+          log("[bletemp] no gatt to disconnect");
         }
+        log("[bletemp] returning to launcher in 2s");
         setTimeout(() => { Bangle.load() }, 2000);
       }
     });
+    log("[bletemp] starting notifications");
     return c.startNotifications();
   }).then(function (d) {
+    log("[bletemp] notifications started");
   }).catch(function (e) {
+    log("[bletemp] error", e);
     E.showAlert("error! " + e).then(function () { Bangle.load(); });
   });
 }
@@ -124,4 +163,6 @@ let macID = settings.bt_coreTemperature_id.split(" ");
 //so you can see timeout
 Bangle.setOptions({backlightTimeout: 0}) // turn off the timeout
 Bangle.setBacklight(1); // keep screen on
+modHS.log("Starting Core Temp app with macID: " + macID[0]);
+log("[bletemp] debug logging enabled:", !!settings.DEBUG);
 getTcore(macID[0]);
